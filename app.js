934d9082e121eb40d14a81351629a4855828cd40
AnnabellePLS <annabellybutton44@gmail.com>
2026-02-25 14:10:23 -0700

    initial commit branch2

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const dns = require("node:dns/promises");
 dns.setServers(["8.8.8.8"]);
 
-const expressLayouts = require ("express-ejs-layouts")
+//part 3
 require("dotenv").config();
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
+const mongoose = require("mongoose"); //added
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+const methodOverride = require("method-override");
 
-app.use(express.urlencoded({extended:true}))
+const connectDB = require("./server/config/db");
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+session({
+secret: "keyboard cat",
+resave: false,
+saveUninitialized: true,
+store: MongoStore.default.create({
+    mongoUrl: process.env.MONGO_URI,
+}),
+//cookie: {maxAge:new Date(Date.now() + 3600000)}
+//Date.now() - 30 * 24 * 60 * 60 * 100
+})
+);
 
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.use("/", require("./server/routes/main"));
-const connectDB = require("./server/config/db");
-connectDB ();
+app.use("/", require("./server/routes/main")); 
+app.use("/", require("./server/routes/admin"));
 
-app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is runnin on port ${PORT}`));
